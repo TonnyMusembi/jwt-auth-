fead770e317a9978a1da7689b7494a291c01500c
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-31 16:00:12 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const con = require("/connection");
 const express = require("express");
 const app = express();
+app.use(express.json());
+const port = 5000
 
 app.get("/", (req, res) => {
     con.query("select *from  students ", (err, result) => {
@@ -10,7 +12,17 @@ app.get("/", (req, res) => {
             res.send(result);
         }
     });
-    app.post('/', (req, res) => {
-        console.log("post api working")
-    })
 });
+app.post("/", (req, res) => {
+    console.log("post api working");
+    con.query("insert into products set ", (err, result) => {
+        if (err) {
+            res.send("Error");
+        } else {
+            res.send(result);
+        }
+    });
+});
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
