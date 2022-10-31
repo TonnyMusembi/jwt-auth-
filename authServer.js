4a236a0661368503c6df3549caf435d9cbbf1477
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-31 14:50:51 +0300

    add

--- authServer.js
+++ authServer.js
@@ -19,3 +19,34 @@ app.post("/createUser", async(req, res) => {
     res.status(201).send(users);
     console.log(users);
 });
+
+const jwt = require("jsonwebtoken");
+
+app.post("/login", async(req, res) => {
+    const user = users.find((c) => c.user == req.body.name);
+    //check to see if the user exists in the list of registered users
+    if (user == null) res.status(404).send("User does not exist!");
+    //if user does not exist, send a 400 response
+    if (await bcrypt.compare(req.body.password, user.password)) {
+        const accessToken = generateAccessToken({ user: req.body.name });
+        const refreshToken = generateRefreshToken({ user: req.body.name });
+        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    } else {
+        res.status(401).send("Password Incorrect!");
+    }
+});
+
+function generateAccessToken(user) {
+    return;
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+}
+// refreshTokens
+let refreshTokens = [];
+
+function generateRefreshToken(user) {
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "20m",
+    });
+    refreshTokens.push(refreshToken);
+    return refreshToken;
+}
